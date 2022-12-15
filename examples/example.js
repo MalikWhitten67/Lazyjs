ad3cf5fd5887447c20cc387e89c8222aecc7c963
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2022-12-15 09:43:02 -0800

    Create example.js

---
+++ examples/example.js
@@ -0,0 +1,11 @@
+if(Add(1,200) > Add(2,200)){
+     'Variable can only be used once',
+     variable(200),
+    
+     trace("yes")
+    
+}
+else{
+    variable(100),
+    trace("Nope", variable * 100)
+}
